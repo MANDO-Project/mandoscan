6cbfe7a4790ce8ba379056a7a29ed595ed8cdbb9
MinhNN <nguyenminh180798@gmail.com>
2025-10-31 11:56:20 +0800

    Add configuration for svg webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'mando-tool',
-    output: 'export',
-  //   webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ["@svgr/webpack"],
-  //   });
-  //   return config;
-  // },
+    // output: 'export',
+
+    webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
