687390f2225a77d2f84f2cdf9d5a89ecaec4229a
MinhNN <nguyenminh180798@gmail.com>
2025-12-10 17:36:34 +0800

    Revert "Add Env Variable to next config file for Amplify"
    This reverts commit 58aaedba32cefe88821b4b5df2ab81abfe8bc043.

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,6 @@ const nextConfig = {
     // Explicitly expose environment variables for Amplify deployment
     env: {
         SCAN_API_BASE_URL: process.env.SCAN_API_BASE_URL,
-        NEXT_PUBLIC_COGNITO_AUTHORITY: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY,
-        NEXT_PUBLIC_COGNITO_CLIENT_ID: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
-        NEXT_PUBLIC_COGNITO_DOMAIN: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
-        NEXT_PUBLIC_REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
-        NEXT_PUBLIC_LOGOUT_URI: process.env.NEXT_PUBLIC_LOGOUT_URI
     },
 
     webpack(config) {
